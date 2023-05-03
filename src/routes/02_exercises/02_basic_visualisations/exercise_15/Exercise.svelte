8646173faaed6a053505ca5a917c3c11f033a5be
Alexander-Deleeck <alexanderdeleeck@hotmail.com>
2023-05-03 15:01:26 +0200

    ex 15 made

--- src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_15/Exercise.svelte
@@ -1,21 +1,32 @@
 <script>
     const width = 100;
     const height = 100;
-    const margin = { top: 5, right: 5, bottom: 5, left: 5 };
+    const margin = { top: 30, right: 30, bottom: 30, left: 30 };
     const innerWidth = width - margin.left - margin.right;
     const innerHeight = height - margin.top - margin.bottom;
   </script>
   
   <svg {width} {height}>
     <g transform="translate({margin.left}, {margin.top})">
-      <!--  -->
+      <rect x=0 y=0 width={innerWidth} height={innerHeight} />
+      <circle cx = {innerWidth} cy={innerHeight} r=20 />
     </g>
+
   </svg>
   
   <style>
     svg {
       border: 1px solid black;
       border-radius: 5px;
+    }
+    rect {
+      fill: yellow;
+      opacity: 0.3;
+      stroke: aqua;
+    }
+    circle {
+      fill: steelblue;
+      opacity: 0.5
     }
   </style>
   
