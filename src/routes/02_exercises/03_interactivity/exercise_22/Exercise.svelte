26c02fdca817bdd3c1f0741c6f6e126a10efdcbd
Alexander-Deleeck <alexanderdeleeck@hotmail.com>
2023-05-03 14:24:58 +0200

    Solved ex 22 Button clicker counter

--- src/routes/02_exercises/03_interactivity/exercise_22/Exercise.svelte
+++ src/routes/02_exercises/03_interactivity/exercise_22/Exercise.svelte
@@ -1,3 +1,14 @@
 <script>
-    
+    let counter = 0
+    function myClicker(event) {
+        counter += 1
+    }
+
+    function myReset(event) {
+        counter = 0
+    }
 </script>
+
+<button on:click={myClicker}> Click here {counter}</button>
+
+<button on:click={myReset}> Reset counter {counter}</button>
