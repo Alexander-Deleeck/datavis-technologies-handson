1ef568f2383642eb85949c76da528c3022aefe0d
Alexander-Deleeck <alexanderdeleeck@hotmail.com>
2023-05-09 15:32:27 +0200

    Ex 21 mostly made, Scatterplot of gapminder data

--- src/routes/02_exercises/02_basic_visualisations/exercise_21/Exercise.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_21/Exercise.svelte
@@ -1,3 +1,18 @@
 <script>
+    import Scatterplot from "./Scatterplot.svelte"; 
+    import {onMount} from 'svelte';
 
+    let data = null; // initialize data object
+    onMount(async() => {
+        const res = await fetch('/data/gapminder.json') // load the data into result
+        let data_temp = await res.json() // convert to json
+
+        data = data_temp.find(datapoint => datapoint.year == '1800')['countries']; // Get data for countries in year 1800
+    })
 </script>
+
+{#if data == null}
+    <p>The data is being loaded</p>
+{:else}
+    <Scatterplot datapoints={data} />
+{/if}
--- src/routes/02_exercises/02_basic_visualisations/exercise_21/Scatterplot.svelte
+++ src/routes/02_exercises/02_basic_visualisations/exercise_21/Scatterplot.svelte
@@ -1,14 +1,58 @@
 <script>
-    // Dimensions
-    const [height, width] = [400, 600];
-    const margin = { top: 50, right: 5, bottom: 55, left: 50 };
-    const innerWidth = width - margin.left - margin.right;
-    const innerHeight = height - margin.top - margin.bottom;
-  </script>
-  
-  <svg viewBox="0 0 {width} {height}" style="max-width: {width}px">
-    <g transform="translate({margin.left}, {margin.top})">
-      <!--  -->
-    </g>
-  </svg>
-  
+  // A datapoint looks like this:
+  // {continent:"europe"
+  // country:"Yugoslavia"
+  // income:null
+  // life_exp:null
+  // population:4687422}
+
+  import { scaleBand, scaleLinear } from 'd3-scale';
+  import { min, max, extent} from "d3-array";
+  import { axisLeft, axisBottom } from 'd3-axis';
+  import { select } from 'd3-selection';
+
+  // Dimensions
+  const [height, width] = [400, 600];
+  const margin = { top: 50, right: 5, bottom: 55, left: 50 };
+  const innerWidth = width - margin.left - margin.right;
+  const innerHeight = height - margin.top - margin.bottom;
+
+  // Data
+  export let datapoints = []; //Because of the export we can access this variable from outside. 
+
+  // Scales
+  const xScale = scaleLinear().domain([0, max(datapoints, d => d.income)]).range([0, innerWidth]);
+  const yScale = scaleLinear().domain([0, max(datapoints, d => d.life_exp)]).range([innerHeight, 0]);
+
+  // Axis
+  function leftAxisBuilder(handle){
+    let axisGen = axisLeft(yScale)
+    let yAxis = axisGen(select(handle))
+  }
+
+  function bottomAxisBuilder(handle){
+    let axisGen = axisBottom(xScale)
+    let xAxis = axisGen(select(handle))
+  }
+</script>
+
+<svg viewBox="0 0 {width} {height}" style="max-width: {width}px">
+  <g transform="translate({margin.left}, {margin.top})">
+    {#each datapoints as datapoint,i}
+        <circle cx={xScale(+datapoint.income)} cy={yScale(+datapoint.life_exp)} r=7></circle>
+    {/each}
+    <g use:leftAxisBuilder></g>
+    <g transform={`translate(0,${innerHeight})`} use:bottomAxisBuilder></g>
+    <text transform="translate({innerWidth / 2}, {innerHeight+35})" >Income</text>
+  </g>
+</svg>
+
+<style>
+  circle{
+    fill: rgb(47, 158, 134);
+    opacity: 0.4;
+  }
+  text{
+    text-anchor: middle;
+  }
+</style>
